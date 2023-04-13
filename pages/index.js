7d21eef75f2fc4dbe220fc1eee07ea29f0453d02
FIDELIS-TUWEI <fideliofidel9@gmail.com>
2023-04-13 17:59:52 +0300

    bug fixed

--- pages/index.js
+++ pages/index.js
@@ -4,7 +4,7 @@ export default function Home({ lessons }) {
   console.log({ lessons });
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      {lessons.map((lesson) => (
+      {lessons?.map((lesson) => (
         <p key={lesson.id}>{lesson.title}</p>
       ))}
     </main>
