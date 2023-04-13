d8d2ce831d9cfd60580d59b13eee463b226f34a8
FIDELIS-TUWEI <fideliofidel9@gmail.com>
2023-04-13 17:26:49 +0300

    index changes

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,20 @@
-export default function Home() {
+import { supabase } from "../utils/supabase"
+
+export default function Home({ lessons }) {
+  console.log({ lessons });
   return (
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       working.
     </main>
   )
 }
+
+export const getStaticProps = async () => {
+  const { data: lessons } = await supabase.from("lesson").select("*")
+
+  return {
+    props: {
+      lessons,
+    },
+  };
+};
