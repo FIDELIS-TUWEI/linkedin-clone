eab2d1d4d9266ae3d7b4f1b91ef7a327fe6f8a27
FIDELIS-TUWEI <fideliofidel9@gmail.com>
2023-04-18 17:10:02 +0300

    scss installed

--- src/components/LoginComponent.jsx
+++ src/components/LoginComponent.jsx
@@ -1,10 +1,11 @@
-import {LoginAPI} from '../api/AuthAPI'
+import {LoginAPI} from '../api/AuthAPI';
+import '../Sass/LoginComponent.scss';
 
 const LoginComponent = () => {
     return ( 
         <div>
             <h1>Login Component</h1>
-            <button>LOGIN to LinkedIn</button>
+            <button className='login-btn'>LOGIN to LinkedIn</button>
         </div>
      );
 }
