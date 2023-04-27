dc2b21442996a02760b9aab796744e6dd87a5345
FIDELIS-TUWEI <fideliofidel9@gmail.com>
2023-04-27 20:54:03 +0300

    useEffect

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,8 +1,16 @@
 import HomeComponent from "../components/HomeComponent";
 import { onAuthStateChanged } from "firebase/auth";
 import { useEffect } from "react";
+import { auth } from "../firebaseConfig";
 
 const Home = () => {
+
+    // useEffect Hook
+    useEffect(() => {
+        onAuthStateChanged(auth, (res) => {
+            console.log(res)
+        });
+    }, []);
     return ( 
         <HomeComponent />
      );
