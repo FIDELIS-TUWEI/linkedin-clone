228bf1ed660a6925917334368bbf391a1688d090
FIDELIS-TUWEI <fideliofidel9@gmail.com>
2023-04-28 06:33:22 +0300

    navigate conditional statement

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,13 +2,19 @@ import HomeComponent from "../components/HomeComponent";
 import { onAuthStateChanged } from "firebase/auth";
 import { useEffect } from "react";
 import { auth } from "../firebaseConfig";
+import { useNavigate } from "react-router-dom";
 
 const Home = () => {
+
+    // navigate hook
+    let navigate = useNavigate();
 
     // useEffect Hook
     useEffect(() => {
         onAuthStateChanged(auth, (res) => {
-            console.log(res?.accessToken)
+            if(!res?.accessToken) {
+                navigate("/")
+            }
         });
     }, []);
     return <HomeComponent />
