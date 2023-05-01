0444be286451007be3a99dcbac6ba8b3109cac90
FIDELIS-TUWEI <fideliofidel9@gmail.com>
2023-05-01 13:44:50 +0300

    react-icon installed

--- src/components/common/Topbar/index.jsx
+++ src/components/common/Topbar/index.jsx
@@ -1,10 +1,12 @@
 import './index.scss'
 import LinkedinLogo from '../../../assets/linkedinLogo.png'
+import { AiOutlineHome } from 'react-icons/ai'
 
 const TopBar = () => {
     return ( 
         <div className='topbar-main'>
             <img className='linkedin-logo' src={LinkedinLogo} alt="LinkedinLogo" />
+            <AiOutlineHome />
         </div>
      );
 }
