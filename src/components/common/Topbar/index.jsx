e0f3721fffec270ac58cb5a2f001e989a6f66064
FIDELIS-TUWEI <fideliofidel9@gmail.com>
2023-05-01 14:34:53 +0300

    user-logo scss react icons - topbar done

--- src/components/common/Topbar/index.jsx
+++ src/components/common/Topbar/index.jsx
@@ -1,12 +1,32 @@
 import './index.scss'
 import LinkedinLogo from '../../../assets/linkedinLogo.png'
-import { AiOutlineHome } from 'react-icons/ai'
+import User from '../../../assets/user.png'
+import { 
+    AiOutlineHome, 
+    AiOutlineSearch, 
+    AiOutlineUserSwitch,
+    AiOutlineMessage,
+    AiOutlineBell 
+} from 'react-icons/ai'
+import { BsBriefcase } from 'react-icons/bs'
 
 const TopBar = () => {
     return ( 
         <div className='topbar-main'>
             <img className='linkedin-logo' src={LinkedinLogo} alt="LinkedinLogo" />
-            <AiOutlineHome />
+
+            {/* React icons */}
+            <div className='react-icons'>
+                <AiOutlineSearch size={30} className='react-icon' />
+                <AiOutlineHome size={30} className='react-icon' />
+                <AiOutlineUserSwitch size={30} className='react-icon' />
+                <BsBriefcase size={30} className='react-icon' />
+                <AiOutlineMessage size={30} className='react-icon' />
+                <AiOutlineBell size={30} className='react-icon' />
+            </div>
+
+            {/* User Icon */}
+            <img className='user-logo' src={User} alt="User" />
         </div>
      );
 }
