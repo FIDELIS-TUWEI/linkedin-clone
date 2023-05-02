dea2a7e52d1a4b4bc7f245c3b06181604da5d662
FIDELIS-TUWEI <fideliofidel9@gmail.com>
2023-05-03 00:16:53 +0300

    FirestoreAPI created /getFirestore initialised form firestore

--- src/firebaseConfig.js
+++ src/firebaseConfig.js
@@ -2,6 +2,7 @@
 import { initializeApp } from "firebase/app";
 import { getAuth } from 'firebase/auth';
 import { getAnalytics } from "firebase/analytics";
+import { getFirestore } from 'firebase/firestore'
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
@@ -22,4 +23,7 @@ const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
 const analytics = getAnalytics(app);
 
-export { auth, app };
+// firestore db
+const firestore = getFirestore(app);
+
+export { auth, app, firestore };
