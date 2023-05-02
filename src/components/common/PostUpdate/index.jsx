c0c6bd76b17286a13e692a8ab49db077a1711f78
FIDELIS-TUWEI <fideliofidel9@gmail.com>
2023-05-02 12:33:30 +0300

    modal button scss

--- src/components/common/PostUpdate/index.jsx
+++ src/components/common/PostUpdate/index.jsx
@@ -3,7 +3,9 @@ import './index.scss'
 const PostStatus = () => {
     return ( 
         <div className='post-status-main'>
-            <div className="post-status"></div>
+            <div className="post-status">
+                <button className='open-post-modal'>Start a Post</button>
+            </div>
         </div>
      );
 }
