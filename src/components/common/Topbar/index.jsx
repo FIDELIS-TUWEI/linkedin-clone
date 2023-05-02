777a564f1e065daccd1a9d20ae899608a0ce61aa
FIDELIS-TUWEI <fideliofidel9@gmail.com>
2023-05-02 11:22:01 +0300

    Home Icon event handler

--- src/components/common/Topbar/index.jsx
+++ src/components/common/Topbar/index.jsx
@@ -8,9 +8,19 @@ import {
     AiOutlineMessage,
     AiOutlineBell 
 } from 'react-icons/ai'
+import { useNavigate } from 'react-router-dom'
 import { BsBriefcase } from 'react-icons/bs'
 
 const TopBar = () => {
+
+    // useNavigate hook
+    let navigate = useNavigate();
+
+    // Home Icon event handler
+    const goTo = (route) => {
+        navigate(route)
+    };
+
     return ( 
         <div className='topbar-main'>
             <img className='linkedin-logo' src={LinkedinLogo} alt="LinkedinLogo" />
@@ -18,8 +28,8 @@ const TopBar = () => {
             {/* React icons */}
             <div className='react-icons'>
                 <AiOutlineSearch size={30} className='react-icon' />
-                <AiOutlineHome size={30} className='react-icon' />
-                <AiOutlineUserSwitch size={30} className='react-icon' />
+                <AiOutlineHome size={30} className='react-icon' onClick={() => goTo('/home')} />
+                <AiOutlineUserSwitch size={30} className='react-icon' onClick={() => goTo('/profile')} />
                 <BsBriefcase size={30} className='react-icon' />
                 <AiOutlineMessage size={30} className='react-icon' />
                 <AiOutlineBell size={30} className='react-icon' />
